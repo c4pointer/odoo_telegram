f04ce1e9064f5669357497cdf3cfd5eb67ca5e17
Oleg Zubak <c4point@gmail.com>
2023-02-20 12:36:29 +0200

    small bug fixed

--- models/bot.py
+++ models/bot.py
@@ -3,8 +3,10 @@ from odoo import models, fields, api
 class Bot(models.Model): 
     _name = 'bot.model'
 
-    name = fields.Char(string = "UAH") 
-    usd= fields.Char(string = "USD")
-    eur= fields.Char(string = "EUR")
-    sell_data = fields.Integer(string = "Sell")
-    buy_data = fields.Integer(string = "Buy")
+    name = fields.Text(string = "Bank") 
+    usd= fields.Text(string = "1")
+    eur= fields.Text(string = "1")
+    sell_data_usd = fields.Float(string = "Sell")
+    buy_data_usd = fields.Float(string = "Buy")
+    sell_data_eur = fields.Float(string = "Sell")
+    buy_data_eur = fields.Float(string = "Buy")
