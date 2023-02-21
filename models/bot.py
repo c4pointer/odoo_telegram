838f2cf7ec07a97eeb23ffaccd400f50023a728b
Oleg Zubak <c4point@gmail.com>
2023-02-21 09:47:46 +0200

    update

--- models/bot.py
+++ models/bot.py
@@ -3,9 +3,9 @@ from odoo import models, fields, api
 class Bot(models.Model): 
     _name = 'bot.model'
 
-    name = fields.Text(string = "Bank") 
-    usd= fields.Text(string = "1")
-    eur= fields.Text(string = "1")
+    name = fields.Text(string = "") 
+    usd= fields.Char(string = "")
+    eur= fields.Char(string = "")
     sell_data_usd = fields.Float(string = "Sell")
     buy_data_usd = fields.Float(string = "Buy")
     sell_data_eur = fields.Float(string = "Sell")
